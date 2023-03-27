c72c32c37efd6c1c3114713b8d1e3a567c908801
Abdulsaboor2004 <125990601+Abdulsaboor2004@users.noreply.github.com>
2023-03-28 04:38:02 +0500

    ALL 5 Assignment
     (Sir, please check 2 and 4 not displaying output)

---
+++ 2.EvenorOdd.js
@@ -0,0 +1,17 @@
+// 2). Create a program that prompts the user to enter a number and then checks whether that number is even or odd. Display the result to the user using an alert box.
+
+let number = prompt("Enter a Number: ");
+
+
+if (number % 2 === 0) {
+    
+    alert(number + " is even");
+    
+} else{
+    
+    alert(number + " is Odd");
+    
+}
+
+// I searched online Understood the Code, but don't know why it isn't working Maybe I try checking by transfering it into an Html file.
+
---
+++ 4.AdultorNot.js
@@ -0,0 +1,19 @@
+// Create a program that asks the user to enter their name and age. If the user’s age is greater than or equal to 18, display a message to the console that says “Welcome [name], you are an adult”. If the user’s age is less than 18, display a message that says “Sorry [name], you are not yet an adult”.
+
+
+
+let name = prompt("Enter your name:");
+
+let age = parseInt(prompt("Enter Your Age: "));
+
+
+if (age >= 18){
+
+    console.log("Welcome" + name + " you're an Adult and can surf");
+} else{
+
+    console.log("Sorry" + name + "you aren't Adult now, wait..");
+}
+
+// Sir, not only number 2, but this one is not running also..
+
